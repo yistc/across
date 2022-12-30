2eea4d26c6c6d66553f7b770ed7ab95e4de3c3bf
yistc <32131698+yistc@users.noreply.github.com>
2022-12-30 17:44:39 +0800

    1

--- scripts/bandwidth.sh
+++ scripts/bandwidth.sh
@@ -19,7 +19,35 @@ TEST_FILES_ALL=(
     https://hnd-jp-ping.vultr.com/vultr.com.1000MB.bin
     https://wa-us-ping.vultr.com/vultr.com.1000MB.bin
 
+    # Racknerd
+    http://lg-sea.racknerd.com/100MB.test
+    http://lg-sea.racknerd.com/1000MB.test
+
+    # Greencloud VPS
+    # https://greencloudvps.com/data-centers.php
+    http://216.244.83.34/download/100mb.zip
+    http://103.121.211.211/500MB.test
+    http://45.14.106.106/500MB.test
+    http://89.187.162.133/1000mb.bin
+    https://mirror.sin11.sg.leaseweb.net/speedtest/1000mb.bin
+    http://43.249.36.49/speedtest/1000mb.bin
+
     # Discord CDN
+    https://cdn.discordapp.com/attachments/1046787596535206010/1058318073431605299/DJI_MINI_3_PRO_-_Cinematic_4K_Video-YouTube.webm
+    https://cdn.discordapp.com/attachments/1046787596535206010/1058318254424195073/LG_4K_DEMO_HDR_2018_60FPS_ELBA-YouTube.webm
+
+    # https://sa.net/speedtest/
+    https://lg-ty1.sa.net/100MB.test
+    https://lg-ty2.sa.net/100MB.test
+
+    # v.ps
+    https://hkg.lg.v.ps/100MB.test
+    https://nrt.lg.v.ps/100MB.test
+    https://kix.lg.v.ps/100MB.test
+    https://sea.lg.v.ps/100MB.test
+
+    # Speedypage
+    https://sg.lg.speedypage.com/100MB.test
 
     # Leaseweb
     http://mirror.hkg10.hk.leaseweb.net/speedtest/1000mb.bin
@@ -31,8 +59,14 @@ TEST_FILES_ALL=(
 TEST_FILES_JP=(
     http://speedtest.tok02.softlayer.com/downloads/test500.zip
     http://mirror.tyo10.jp.leaseweb.net/speedtest/1000mb.bin
+    https://nrt.lg.v.ps/100MB.test
     https://hnd-jp-ping.vultr.com/vultr.com.1000MB.bin
+    https://kix.lg.v.ps/100MB.test
+    http://45.14.106.106/500MB.test
     http://speedtest.tokyo2.linode.com/100MB-tokyo2.bin
+    https://lg-ty1.sa.net/100MB.test
+    https://lg-ty2.sa.net/100MB.test
+    http://103.121.211.211/500MB.test
 )
 
 for i in {1..10}; do
