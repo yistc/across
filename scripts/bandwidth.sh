081b89dc09e7aee30f4b69eef9b72db4e12fd635
yistc <32131698+yistc@users.noreply.github.com>
2022-12-30 17:28:21 +0800

    Modify bandwidth.sh

--- scripts/bandwidth.sh
+++ scripts/bandwidth.sh
@@ -1,14 +1,8 @@
 #! /bin/bash
 
-TEST_FILES=(
+TEST_FILES_ALL=(
     # softlayer
-    http://speedtest.sea01.softlayer.com/downloads/test500.zip
-    http://speedtest.sjc01.softlayer.com/downloads/test500.zip
-    http://speedtest.hkg02.softlayer.com/downloads/test500.zip
     http://speedtest.tok02.softlayer.com/downloads/test500.zip
-
-    # hetzner
-    https://speed.hetzner.de/100MB.bin
     
     # Digital Ocean
     http://speedtest-sgp1.digitalocean.com/5gb.test
@@ -17,6 +11,7 @@ TEST_FILES=(
     # Linode
     http://speedtest.ap-south-1.linodeobjects.com/1GB_test.file
     http://speedtest.ap-south-1.linodeobjects.com/10GB_test.file
+    http://speedtest.tokyo2.linode.com/100MB-tokyo2.bin
 
     # Vultr
     https://sgp-ping.vultr.com/vultr.com.1000MB.bin
@@ -32,7 +27,16 @@ TEST_FILES=(
     http://mirror.sfo12.us.leaseweb.net/speedtest/1000mb.bin
 )
 
-for file in ${TEST_FILES[@]}; do
-    echo "Testing $file"
-    wget -O /dev/null $file
+TEST_FILES_JP = (
+    http://speedtest.tok02.softlayer.com/downloads/test500.zip
+    http://mirror.tyo10.jp.leaseweb.net/speedtest/1000mb.bin
+    https://hnd-jp-ping.vultr.com/vultr.com.1000MB.bin
+    http://speedtest.tokyo2.linode.com/100MB-tokyo2.bin
+)
+
+for i in {1..10}; do
+    for file in ${TEST_FILES_JP[@]}; do
+        echo "Testing $file"
+        wget -O /dev/null $file
+    done
 done
